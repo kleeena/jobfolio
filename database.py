cfd8d59c9357aca38bfe3b0e24d91a049890d970
kleeena <62442533+kleeena@users.noreply.github.com>
2025-07-02 15:14:41 +0500

    refactor(db): add safe DB init and session dependency for SQLite
    - Prevent overwriting existing SQLite database by checking file before creating tables
    - Define `get_session()` using context manager for safe session handling
    - Create reusable `SessionDep` using Annotated and Depends
    - Import models (User, Resume, JobApplication) for metadata registration
    - Remove direct `create_all()` call from top-level

--- database.py
+++ database.py
@@ -1,9 +1,25 @@
-from sqlmodel import Field, SQLModel, create_engine
+from fastapi import Depends
+import os
+from sqlmodel import Field, SQLModel, create_engine, Session
 from models import User, Resume, JobApplication
+from typing import Annotated
 
 SQLITE_FILE_NAME = 'database.db'
 SQLITE_URL = f"sqlite:///{SQLITE_FILE_NAME}"
 
 engine = create_engine(SQLITE_URL, echo=True)
-SQLModel.metadata.create_all(engine)
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def create_db_tables(engine):
+    try:
+        if 'database.db' not in os.listdir():
+            SQLModel.metadata.create_all(engine)
+    except:
+        raise('A problem occurred while building the database.')
+    
+SessionDep = Annotated[Session, Depends(get_session)]
+
+create_db_tables(engine)
